93618b91df6d9c67be7c2e7d1735ae3c188e326c
Victor Paredes <141671@unsaac.edu.pe>
2025-11-30 17:37:31 -0500

    CI

---
+++ Function1.cs
@@ -0,0 +1,26 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+public class Function1
+{
+    private readonly ILogger<Function1> _logger;
+
+    public Function1(ILogger<Function1> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("HolaMundo")]
+    public HttpResponseData Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
+    {
+        _logger.LogInformation("Procesando petición HolaMundo");
+        
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.WriteString("¡Hola Mundo!");
+        
+        return response;
+    }
+}
